c21d26b9f4aacb216a97d63237439ffe3287ff21
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-09-15 23:50:31 -0700

    Initial Commit

---
+++ ImgurImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImgurImages
+//
+//  Created by Ronald Hernandez on 9/15/15.
+//  Copyright (c) 2015 Hardcoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImgurImages/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImgurImages
+//
+//  Created by Ronald Hernandez on 9/15/15.
+//  Copyright (c) 2015 Hardcoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
